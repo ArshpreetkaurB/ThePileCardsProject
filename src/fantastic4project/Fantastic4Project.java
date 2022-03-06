a2e6f2d26054bd54f6f5a04dbf126228779eb2ed
Arshpreet Kaur <kau11045@sheridancollege.ca>
2022-03-06 16:24:19 -0500

    test project to git hub

---
+++ src/fantastic4project/Fantastic4Project.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fantastic4project;
+
+/**
+ *
+ * @author hp
+ */
+public class Fantastic4Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // pushing 
+        // testing the project over here 
+    }
+    
+}
